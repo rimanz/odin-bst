0cecf7e543aab6f3cb85faaf6d38bcbb7d21c519
Riman Das <34216663+rimanz@users.noreply.github.com>
2026-08-27 07:39:39 +0600

    Add prettyPrint method to BST class
    - the prettyPrint method provides a visual representation of the tree.

--- src/bst.js
+++ src/bst.js
@@ -23,4 +23,14 @@ export default class Tree {
 
     return root;
   }
+
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null || node === undefined) {
+      return;
+    }
+
+    this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
 }
