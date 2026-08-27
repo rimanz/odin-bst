627771fcda29d8dbedd9ecf6dc5224c2b94f00c4
Riman Das <34216663+rimanz@users.noreply.github.com>
2026-08-27 07:22:48 +0600

    Add Tree class with a buildTree method
    - A Tree class has been added, which has a method called buildTree.
    - The buildTree method takes an array of values, constructs a
    balanced binary search tree (BST) from those values and then returns
    the root node of the tree.

--- src/bst.js
+++ src/bst.js
@@ -5,3 +5,22 @@ class Node {
     this.right = null;
   }
 }
+
+export default class Tree {
+  constructor(arr) {
+    this.root = this.buildTree(arr);
+  }
+
+  buildTree(arr) {
+    arr = Array.from(new Set(arr)).toSorted((a, b) => a - b);
+    if (arr.length === 0) return null;
+
+    const mid = Math.floor(arr.length / 2);
+    const root = new Node(arr[mid]);
+
+    root.left = this.buildTree(arr.slice(0, mid));
+    root.right = this.buildTree(arr.slice(mid + 1));
+
+    return root;
+  }
+}
