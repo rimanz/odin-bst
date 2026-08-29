3280ae988789393a8f90cbc2eb2b6f1c1f11ac42
Riman Das <34216663+rimanz@users.noreply.github.com>
2026-08-29 13:00:57 +0600

    Add queue implementation and tests
    - A basic queue has been implemented to support FIFO operations in the
    Tree methods to facilitate level order traversal.
    - The queue currently supports enqueue, dequeue, peek, size and isEmpty
    operations.
    - It's corresponding test cases have been added in src/queue.test.js.

---
+++ src/queue.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+export default class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  isEmpty() {
+    if (this.tail && this.head === null) {
+      // Exceptional case
+      throw new Error("Invalid Queue: Something Exceptional Hppened!");
+    }
+
+    return this.head === null && this.tail === null;
+  }
+
+  enqueue(data) {
+    const node = new Node(data);
+
+    this.isEmpty() ? (this.head = node) : (this.tail.next = node);
+    this.tail = node;
+    this.length++;
+  }
+
+  dequeue() {
+    const x = this.head;
+
+    this.head = x.next;
+    this.length--;
+
+    return x;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  buildQueue(arr) {
+    arr.forEach((item) => this.enqueue(item));
+  }
+
+  toString() {
+    const outputs = [];
+    let current = this.head;
+
+    while (current) {
+      outputs.push(`( ${current.data} )`);
+      current = current.next;
+    }
+
+    return outputs.join(" <= ");
+    console.log(output);
+  }
+}
---
+++ src/queue.test.js
@@ -0,0 +1,63 @@
+import { describe, expect, test } from "@jest/globals";
+import Queue from "./queue.js";
+
+const arr = [1, 7, 4, 23];
+const queue = new Queue();
+queue.buildQueue(arr);
+
+describe("Queue", () => {
+  test("Queue class should exist!", () => {
+    expect(queue).toBeDefined();
+  });
+
+  test("The value of queue.head.data should be 1", () => {
+    const output = queue.head.data;
+    const expected = 1;
+
+    expect(output).toBe(expected);
+  });
+
+  test("The value of queue.tail.data should be 23", () => {
+    const output = queue.tail.data;
+    const expected = 23;
+
+    expect(output).toBe(expected);
+  });
+
+  test("After adding 9, the value of queue.tail.data should be 9", () => {
+    queue.enqueue(9);
+    const output = queue.tail.data;
+    const expected = 9;
+
+    expect(output).toBe(expected);
+  });
+
+  test("After popping, popped data should return", () => {
+    const popped = queue.dequeue();
+    const output = popped.data;
+    const expected = 1;
+
+    expect(output).toBe(expected);
+  });
+
+  test("Now the value of queue.head.data should be 7", () => {
+    const output = queue.head.data;
+    const expected = 7;
+
+    expect(output).toBe(expected);
+  });
+
+  test("Now the size of the queue should be 4", () => {
+    const output = queue.size();
+    const expected = 4;
+
+    expect(output).toBe(expected);
+  });
+
+  test("Calling 'queue.toString' should return ( 7 ) <= ( 4 ) <= ( 23 ) <= ( 9 )'", () => {
+    const output = queue.toString();
+    const expected = "( 7 ) <= ( 4 ) <= ( 23 ) <= ( 9 )";
+
+    expect(output).toBe(expected);
+  });
+});
