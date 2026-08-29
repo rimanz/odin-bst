5aabb55218553db8b7f58f266fc9d03161e94548
Riman Das <34216663+rimanz@users.noreply.github.com>
2026-08-29 15:22:10 +0600

    Add test test cases for bst.js

---
+++ src/bst.test.js
@@ -0,0 +1,61 @@
+import { describe, expect, test } from "@jest/globals";
+import Tree from "./bst.js";
+
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(arr);
+
+describe("Binary Search Tree", () => {
+  test("Node class should exist!", () => {
+    expect(Tree).toBeDefined();
+  });
+
+  test("The value of tree.root.data should be 8", () => {
+    const output = tree.root.data;
+    const expected = 8;
+
+    expect(output).toBe(expected);
+  });
+
+  test("The value of tree.root.data should be 8", () => {
+    const output = tree.root.data;
+    const expected = 8;
+
+    expect(output).toBe(expected);
+  });
+
+  test("The value of tree.root.left.data should be 4", () => {
+    const output = tree.root.left.data;
+    const expected = 4;
+
+    expect(output).toBe(expected);
+  });
+
+  test("The value of tree.root.right.data should be 67", () => {
+    const output = tree.root.right.data;
+    const expected = 67;
+
+    expect(output).toBe(expected);
+  });
+
+  test('tree.includes(9) should return "true"', () => {
+    const output = tree.includes(9);
+    const expected = true;
+
+    expect(output).toBe(expected);
+  });
+
+  test('tree.includes(19) should return "false"', () => {
+    const output = tree.includes(19);
+    const expected = false;
+
+    expect(output).toBe(expected);
+  });
+
+  test('After inserting 19, tree.includes(19) should return "true"', () => {
+    tree.insert(19);
+    const output = tree.includes(19);
+    const expected = true;
+
+    expect(output).toBe(expected);
+  });
+});
